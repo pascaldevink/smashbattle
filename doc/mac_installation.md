1f4c263aef2a15fca942f3b6d171ea15be7b0dce
Pascal de Vink <pascal.de.vink@gmail.com>
2014-03-31 21:08:07 +0200

    Finished mac support with a better launcher, a backup install script and some more documentation.

--- doc/mac_installation.md
+++ doc/mac_installation.md
@@ -2,11 +2,15 @@
 
 ## Install prerequisites
 
-    brew install sdl sdl_net sdl_mixer
+    https://xquartz.macosforge.org
+
+    brew install sdl --universal --with-x11-driver
+    brew install sdl_net --universal
+    brew install sdl_mixer --universal --with-libvorbis
 
 ## Building
 
-    cmake -DCMAKE_CXX_FLAGS=-Wno-narrowing .
+    cmake -DCMAKE_CXX_FLAGS=-Wno-narrowing -DCMAKE_EXE_LINKER_FLAGS=-Wl,-headerpad_max_install_names .
     make
 
 ## Installation
@@ -19,6 +23,6 @@
 
 ## About the application bundle
 
-* Both all the actual game files and the SDL libraries go into SmashBattle.app/Contents/MacOS/
-* The name of the app, icon and executable are defined in the Info.plist. This is copied from scripts/mac
-* The icon(s) are in the SB.icns in the SmashBattle.app/Contents/Resources folder. This is copied from scripts/mac
+* Both the actual game files and the SDL libraries go into SmashBattle.app/Contents/MacOS/
+* The name of the app, icon and executable are defined in the Info.plist. This is copied from scripts/mac/
+* The icon(s) are in the SB.icns in the SmashBattle.app/Contents/Resources/ folder. This is copied from scripts/mac/
--- mac_application_bundle.sh
+++ mac_application_bundle.sh
@@ -1,5 +1,16 @@
 #!/bin/sh
 
+#
+# This script creates an application bundle in the current directory based on
+# the locally installed game. Make sure you compiled and installed before
+# running this script.
+#
+# Also, SDL should be installed in /usr/local. If not, change the location in
+# the lines below
+#
+# Based on http://stackoverflow.com/questions/1596945/building-osx-app-bundle
+#
+
 # Create application bundle
 mkdir -p SmashBattle.app/Contents/MacOS
 mkdir -p SmashBattle.app/Contents/Resources
@@ -12,10 +23,18 @@ cp /usr/local/lib/libSDL-1.2.0.dylib SmashBattle.app/Contents/MacOS
 cp /usr/local/lib/libSDL_mixer-1.2.0.dylib SmashBattle.app/Contents/MacOS
 cp /usr/local/lib/libSDL_net-1.2.0.dylib SmashBattle.app/Contents/MacOS
 
+chmod 766 SmashBattle.app/Contents/MacOS/libSDL*
+
 # Change location of linked libraries
 install_name_tool -change /usr/local/lib/libSDL-1.2.0.dylib @executable_path/libSDL-1.2.0.dylib SmashBattle.app/Contents/MacOS/smashbattle
+install_name_tool -change /usr/local/lib/libSDL-1.2.0.dylib @executable_path/libSDL-1.2.0.dylib SmashBattle.app/Contents/MacOS/libSDL-1.2.0.dylib
+install_name_tool -change /usr/local/lib/libSDL-1.2.0.dylib @executable_path/libSDL-1.2.0.dylib SmashBattle.app/Contents/MacOS/libSDL_mixer-1.2.0.dylib
+install_name_tool -change /usr/local/lib/libSDL-1.2.0.dylib @executable_path/libSDL-1.2.0.dylib SmashBattle.app/Contents/MacOS/libSDL_net-1.2.0.dylib
 install_name_tool -change /usr/local/lib/libSDL_mixer-1.2.0.dylib @executable_path/libSDL_mixer-1.2.0.dylib SmashBattle.app/Contents/MacOS/smashbattle
 install_name_tool -change /usr/local/lib/libSDL_net-1.2.0.dylib @executable_path/libSDL_net-1.2.0.dylib SmashBattle.app/Contents/MacOS/smashbattle
+
+# Copy launcher into application bundle
+cp scripts/mac/launcher SmashBattle.app/Contents/MacOS
 
 # Copy icons into application bundle
 cp scripts/mac/SB.icns SmashBattle.app/Contents/Resources
